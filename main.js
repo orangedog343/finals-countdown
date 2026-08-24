7f5eabc9f843567a3564914a453ab230899c9b34
Aarav Kumar <orangedog343mc@gmail.com>
2026-08-25 00:45:53 +0530

    Moved paper details to json, dynamically generated html for each subject and paper, to make adding new papers/subjects easier, and cleaned up code

--- main.js
+++ main.js
@@ -1,27 +1,41 @@
-const old_papers = {
-    "0653p2" : new Date("2026-03-02T09:00:00+05:30"),
-    "0653p4" : new Date("2026-03-02T10:45:00+05:30"),
-    "0653p6" : new Date("2026-03-03T09:00:00+05:30"),
-    "0580p2" : new Date("2026-03-06T09:00:00+05:30"),
-    "0580p4" : new Date("2026-03-07T10:00:00+05:30"),
-    "fifth"  : new Date("2026-03-04T09:00:00+05:30"),
-    "kanp1"  : new Date("2026-03-05T09:00:00+05:30"),
-    "kanp2"  : new Date("2026-03-05T10:30:00+05:30"),
-}
-const papers = {
-  "0500p1" : new Date("2026-09-25T09:00:00+05:30"),
-  "0580p2" : new Date("2026-09-28T09:00:00+05:30"),
-  "0580p4" : new Date("2026-09-29T09:00:00+05:30"),
-  "0680p1" : new Date("2026-09-30T09:00:00+05:30"),
-  "0680p2" : new Date("2026-10-01T09:00:00+05:30"),
-  "0654p2" : new Date("2026-10-05T09:00:00+05:30"),
-  "0654p4" : new Date("2026-10-05T10:30:00+05:30"),
-  "0654p6" : new Date("2026-10-06T09:00:00+05:30"),
-  "0457p2" : new Date("2026-10-07T09:00:00+05:30"),
-  "0500p2" : new Date("2026-10-07T13:00:00+05:30"),
-}
+const dates = [];
+let countNumbers = [];
+async function loadPapers() {
+    const response = await fetch("papers.json");
+    const papers = await response.json();
 
-const paperIDs = {}
+    displayPapers(papers);
+}
+function displayPapers(papers){
+  let html = "";
+  for (const subject in papers){
+    let sub_obj = papers[subject];
+    html += `<div class="subject" style="--h : ${sub_obj.color.hue}; --s : ${sub_obj.color.saturation}%;">\n`;
+    html += `<h1>${subject}</h1>\n`;
+    for (const paper in sub_obj.papers){
+      let paper_dat = sub_obj.papers[paper];
+      const date_str = formatDate(new Date(paper_dat.time))
+      const paper_name = paper_dat.name === undefined ? `Paper ${paper}` : `Paper ${paper} (${paper_dat.name})`
+      html += `<div class="paper" id="${sub_obj.code}p${paper}">
+                    <div class="paper-details">
+                        <div class="paper-name">${paper_name}</div>
+                        <div class="paper-date">${date_str}</div>
+                    </div>
+                    <div class="countdown">
+                        <div class="count-number">-</div>
+                        <div class="count-label">days to go</div>
+                    </div>
+                </div>
+`;
+      dates.push(new Date(paper_dat.time));
+    }
+    html += "</div>\n";
+  }
+  console.log(`Generated HTML : \n ${html}`);
+  document.getElementById("subjects").innerHTML = html;
+  countNumbers = document.querySelectorAll(".count-number");
+  updatePapers();
+}
 function formatDate(date){
     const day = date.getDate();
     const weekday = date.toLocaleDateString("en-GB", {weekday : "short"});
@@ -45,21 +59,15 @@ function daysLeft(date){
     return Math.floor(diffMs / (24 * 60 * 60 * 1000));
 }
 function updatePaper(id){
-    paperIDs[id].textContent = daysLeft(papers[id]);
+    countNumbers[id].textContent = daysLeft(dates[id]);
 }
 function updatePapers(){
-    Object.keys(papers).forEach(key => {
-        updatePaper(key);
-    })
+    for (let i = 0; i < dates.length; i++){
+        updatePaper(i);
+  }
     console.log("Updated papers");
     setTimeout(updatePapers, 10*1000);
 }
 document.addEventListener("DOMContentLoaded", ()=> {
-    Object.keys(papers).forEach(key => {
-        const paper_div = document.getElementById(key);
-        paperIDs[key] = paper_div.querySelector(".count-number");
-        paper_div.querySelector(".paper-date").textContent = formatDate(papers[key])
-        console.log(formatDate(papers[key]));
-    });
-    updatePapers();
+    loadPapers();
 });
